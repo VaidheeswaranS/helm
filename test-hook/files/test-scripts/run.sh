0e76ad4d33b52d59a7b006dd7a2d6bd33633772f
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2025-10-08 22:29:42 +0530

    feat: added the example of using test hooks in helm

---
+++ test-hook/files/test-scripts/run.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/sh
+
+echo "Installing the curl command"
+apk update && apk add --no-cache curl
+
+echo "Executing the curl request to {{ .Release.Name }}"
+curl http://{{ include "test-hook.fullname" . }}:{{ .Values.service.port }}
